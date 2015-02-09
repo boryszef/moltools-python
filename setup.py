f2d75d06e49bf46a038008f150f6e92b63808d44
boryszef@gmail.com <boryszef@gmail.com@81d03d5e-80ac-99c1-ca8c-505e79a511f1>
2015-02-09 17:45:20 +0000

    topology tools moved to topology.c

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from numpy.distutils.core import setup, Extension
 
 moltools = Extension('moltools',
 	sources = [ 'eam.c', 'periodic_table.c', 'utils.c', 'readers.c',
-				'measure.c', 'constants.c', 'writers.c', 'energy.c',
+	            'measure.c', 'constants.c', 'writers.c', 'topology.c',
 	            'molecule.c', 'moltools.c', ],
 	extra_compile_args=["-g"],
 	extra_link_args=["-g"]
