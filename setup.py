e0231802575369c44a9c23fe6b721ea0c30d7eab
Borys Szefczyk <boryszef@gmail.com>
2016-02-05 08:30:02 +0100

    Working on conditional packages in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from numpy.distutils.core import setup, Extension
 
+# Check if gromacs is present
+if extraPackagePresent(libgromacs):
+    cflags =
+    libs =
+
 moltools = Extension('moltools',
 	sources = [ 'eam.c', 'periodic_table.c', 'utils.c', 'readers.c',
 	            'measure.c', 'constants.c', 'writers.c', 'topology.c',
@@ -8,6 +13,8 @@ moltools = Extension('moltools',
 	#extra_link_args=["-O0 -g"]
 )
 
-setup (name = 'moltools-python', version = '0.1',
+setup (name = 'moltools-python', version = '0.1.1',
        description = 'Python module for manipulation of atomic coordinates',
+       author = 'Borys Szefczyk', author_email = 'boryszef@gmail.com',
+       url = 'https://github.com/boryszef/moltools-python',
        ext_modules = [ moltools ] )
