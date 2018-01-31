169d94ebe47d91ffa3d59481b797380a360a9989
Borys Szefczyk <boryszef@gmail.com>
2018-01-31 21:15:57 +0100

    Progress on topology tests

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -6,12 +6,17 @@ class TestTopology(unittest.TestCase):
 
     def setUp(self):
 
-        systems = [ '3mols.xyz', 'chex.xyz', 'nh3h2so4.xyz' ]
+        systems = [ '3mols.xyz', 'pyr.xyz', ]#'nh3h2so4.xyz' ]
 
         self.expected = {
             '3mols.xyz' : [
                 [ (0, 1), (0, 2), (0, 3), (0, 4), (5, 6), (5, 7) ],
                 [ [1,2,3,4], [0], [0], [0], [0], [6,7], [5], [5], [] ] ],
+            'pyr.xyz' : [
+                [ (0, 1), (0, 4), (0, 9), (1, 2), (1, 8), (2, 3), (2, 7),
+                  (3, 4), (3, 6), (4, 5) ],
+                [ [1,4,9], [0,2,8], [1,3,7], [2,4,6], [0,3,5], [4], [3],
+                  [2], [1], [0] ] ]
         }
 
         self.testDir = os.path.dirname(os.path.realpath(__file__))
@@ -31,13 +36,12 @@ class TestTopology(unittest.TestCase):
         for k, v in self.systems.items():
             bonds = mdarray.findBonds(v['symbols'], v['crd'])
             print(k, bonds)
-            #self.assertEqual(bonds, expected)
+            self.assertEqual(bonds, self.expected[k][0])
 
             bonds = mdarray.findBonds(v['symbols'],
                 v['crd'], format="atoms")
             print(k, bonds)
-
-            #self.assertEqual(bonds, expected)
+            self.assertEqual(bonds, self.expected[k][1])
 
     def test_findMolecules(self):
 
